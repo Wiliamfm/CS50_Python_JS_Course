46a0605d8355ff7bfe97d01372389d23d1827cad
William <santifon15@gmail.com>
2023-07-07 20:01:47 -0500

    add logout feature

--- capstone/veterinary/urls.py
+++ capstone/veterinary/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     path("register/", views.register, name="register"),
     path('login/', auth_views.LoginView.as_view(
         template_name='veterinary/login.html',
-        next_page='veterinary/Make-Appointment.html'),
-        name="login"),
-    path('logout/', auth_views.LogoutView.as_view(template_name='veterianry/logout.html'), name='logout'),
+        next_page='todo'), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(
+        template_name='veterianry/logout.html',
+        next_page='login'), name='logout'),
 ]
